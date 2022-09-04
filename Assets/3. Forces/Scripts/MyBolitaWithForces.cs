4f8856b6a5e458adc22a9df713dad1bc01532eaa
Valentina <valentina.francoa@upb.edu.co>
2022-09-03 19:04:02 -0500

    Fluid Friction

--- Assets/3. Forces/Scripts/MyBolitaWithForces.cs
+++ Assets/3. Forces/Scripts/MyBolitaWithForces.cs
@@ -5,7 +5,15 @@ using UnityEngine;
 
 public class MyBolitaWithForces : MonoBehaviour
 {
+    public enum BolitaRunMode
+    {
+        Friction,
+        FluidFriction,
+        Gravity
+    }
+
     private MyVector2D position;
+    [SerializeField] private BolitaRunMode runMode;
     [SerializeField] private MyVector2D velocity;
     [SerializeField] private MyVector2D acceleration;
     [SerializeField] private float mass = 1f;
@@ -16,9 +24,9 @@ public class MyBolitaWithForces : MonoBehaviour
     private MyVector2D netForce;
     private MyVector2D weight;
 
-    [Range(0f,1f)] [SerializeField] private float dampingFactor;
-    [Range(0f, 1f)] [SerializeField] private float frictionCoeficent;
-    [SerializeField] Camera camara;
+    [SerializeField] private Camera camara;
+    [Range(0f,1f)] [SerializeField] private float dampingFactor=0.9f;
+    [Range(0f, 1f)] [SerializeField] private float frictionCoeficent=0.9f;
 
     void Start()
     {
@@ -27,19 +35,33 @@ public class MyBolitaWithForces : MonoBehaviour
 
     private void FixedUpdate()
     {
+        acceleration*=0f;
         netForce = new MyVector2D(0, 0); //cada vez que inicia receteamos net force para que no incremente a lo loco
+        if (runMode != BolitaRunMode.Gravity)
+        {
+            //weight
+            weight = mass * gravity;
+            ApplyForce(weight);
+        }
 
-        //weight
-        weight = mass * gravity;
-        ApplyForce(weight);
+        if (runMode == BolitaRunMode.FluidFriction)
+        {
+            //Fluid Friction
+            if (transform.position.y <= 0.9)
+            ApplyFluidFriction();
+        }
+        else if(runMode == BolitaRunMode.Friction)
+        {
+            //Friction
+            ApplyFriction();
 
-        //Friction
-        MyVector2D friction = -frictionCoeficent * weight.magnitude * velocity.normalized;
-        ApplyForce(friction);
-        friction.Draw(position, Color.black);
+        }else if (runMode == BolitaRunMode.Gravity)
+        {
+
+        }
 
         //wind
-        ApplyForce(wind);
+        //ApplyForce(wind);
 
         Move(); //lo pongo aqui para tener un deltatime fijo
     }
@@ -47,7 +69,6 @@ public class MyBolitaWithForces : MonoBehaviour
     {
         position.Draw(Color.blue);
         velocity.Draw(position, Color.red); //position indica que el origen del vector va a ser siempre en la bolita
-        //acceleration.Draw(position, Color.green); 
     }
 
     public void Move()
@@ -56,22 +77,11 @@ public class MyBolitaWithForces : MonoBehaviour
         velocity = velocity + acceleration * Time.fixedDeltaTime;
         position = position + velocity * Time.fixedDeltaTime; //(1f / 60f) time.delta time representa esta division 
 
-        //check horizontal bounds
-        if (Mathf.Abs(position.x) > camara.orthographicSize) 
+        if(runMode!= BolitaRunMode.Gravity)
         {
-            velocity.x = velocity.x * -1;
-            position.x = Mathf.Sign(position.x) * camara.orthographicSize;
-            //Mathf.Sign devuelve 1(positivo) 0 -1(negativo) dependiendo del signo 
-            velocity *= dampingFactor; 
+             CheckWolrdBoxBounds();
         }
 
-        //check vertical bounds
-        if (Mathf.Abs(position.y) > camara.orthographicSize) 
-        {
-            velocity.y = velocity.y * -1;
-            position.y = Mathf.Sign(position.y) * camara.orthographicSize;
-            velocity *= dampingFactor;
-        }
         transform.position = new Vector3(position.x, position.y);//referenciar el mov de la bolita para que se mueva con los vectores
     }
 
@@ -79,5 +89,47 @@ public class MyBolitaWithForces : MonoBehaviour
     {
          netForce += force;
          acceleration = netForce / mass;
+    }
+
+    private void ApplyFriction()
+    {
+        MyVector2D friction = -frictionCoeficent * weight.magnitude * velocity.normalized;
+        ApplyForce(friction);
+        friction.Draw(position, Color.black);
+    }
+
+    private void ApplyFluidFriction()
+    {
+        if (transform.localPosition.y <= 0)
+        {
+            float p = 1;//densidad
+            float flontalArea = transform.localScale.x; //A
+            float fluidDragCoefficent = 1; //C
+            float velocityMagnitude = velocity.magnitude; //se obtiene el tamaño, ya que no se permite multiplicar vectores pero si magnitudes
+            float scalarPart = -0.5f * p * velocityMagnitude * velocityMagnitude * flontalArea * fluidDragCoefficent; //ESCALARES FORMULA
+            MyVector2D friction = scalarPart * velocity.normalized;
+
+            ApplyForce(friction);
+        }
+    }
+
+    private void CheckWolrdBoxBounds()
+    {
+        //check horizontal bounds
+        if (Mathf.Abs(position.x) > camara.orthographicSize)
+        {
+            velocity.x = velocity.x * -1;
+            position.x = Mathf.Sign(position.x) * camara.orthographicSize;
+            //Mathf.Sign devuelve 1(positivo) 0 -1(negativo) dependiendo del signo 
+            velocity *= dampingFactor;
+        }
+
+        //check vertical bounds
+        if (Mathf.Abs(position.y) > camara.orthographicSize)
+        {
+            velocity.y = velocity.y * -1;
+            position.y = Mathf.Sign(position.y) * camara.orthographicSize;
+            velocity *= dampingFactor;
+        }
     }
 }
