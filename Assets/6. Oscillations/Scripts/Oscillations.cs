28edaba1340651895c896bcb77e585839f58ed76
Valentina <valentina.francoa@upb.edu.co>
2022-10-13 01:30:30 -0500

    osccillations

--- Assets/6. Oscillations/Scripts/Oscillations.cs
+++ Assets/6. Oscillations/Scripts/Oscillations.cs
@@ -15,11 +15,11 @@ public class Oscillations : MonoBehaviour
     private void Update()
     {
         //PRIMER EJERCICIO
-        //float x = amplitud * Mathf.Sin(2f * Mathf.PI*(Time.time/period));
-        //transform.position = initialPosition + new Vector3(x, x, 0);
+        float x = amplitud * Mathf.Sin(2f * Mathf.PI * (Time.time / period));
+        transform.position = initialPosition + new Vector3(x, x, 0);
 
         //SEGUNDO EJERCICIO
-        float x = Mathf.Sin(5f * Time.time) + Mathf.Cos(Time.time / 3f) + Mathf.Sin(Time.time / 13f);
-        transform.position = initialPosition + new Vector3(x, 0, 0);
+        //float x = Mathf.Sin(5f * Time.time) + Mathf.Cos(Time.time / 3f) + Mathf.Sin(Time.time / 13f);
+        //transform.position = initialPosition + new Vector3(x, 0, 0);
     }
 }
