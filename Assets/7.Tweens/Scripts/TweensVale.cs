16f6760844fa75e40de68d939e598b40f1ac773a
Valentina <valentina.francoa@upb.edu.co>
2022-10-13 17:32:59 -0500

    Tweens

---
+++ Assets/7.Tweens/Scripts/TweensVale.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TweensVale : MonoBehaviour
+{
+    [SerializeField] private Transform targetTransform;
+    [SerializeField, Range(0, 1)] private float normalizedTime;
+    [SerializeField] private float duration=1;
+
+    private float currentTime = 0;
+    private Vector3 initialPosition;
+    private Vector3 finalPosition;
+    [SerializeField] private Color initialColor;
+    [SerializeField] private Color finalColor;
+    private SpriteRenderer spriteRenderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        StartTween();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        normalizedTime = currentTime / duration;
+        transform.position = Vector3.Lerp(initialPosition, finalPosition, normalizedTime);
+        spriteRenderer.color = Color.Lerp(initialColor, finalColor, normalizedTime);
+        currentTime += Time.deltaTime;
+
+        if ( normalizedTime>=1)
+        {
+            Debug.Log("Completed");
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space)) StartTween();
+    }
+
+    private void StartTween()
+    {
+        currentTime = 0f;
+        initialPosition = transform.position;
+        finalPosition = targetTransform.position;
+    }
+}
