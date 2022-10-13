5ceed9868067e998eba9de69ed98292aac164064
Valentina <valentina.francoa@upb.edu.co>
2022-10-13 01:03:02 -0500

    grafica

---
+++ Assets/6. Oscillations/Scripts/Graph.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Graph : MonoBehaviour
+{
+    [SerializeField] int m_totalPoints = 10;
+    [SerializeField] float m_distanceFactor = 10;
+    [SerializeField] GameObject m_prefab;
+    private GameObject[] m_allPoints;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_allPoints = new GameObject[m_totalPoints];
+        for (int i = 0; i < m_totalPoints; ++i)
+        {
+            m_allPoints[i] = Instantiate(m_prefab, transform); 
+        }
+    }
+
+    void Update()
+    {
+        for (int i = 0; i < m_allPoints.Length; ++i)
+        {      
+            float x = i * m_distanceFactor;
+            float y = Mathf.Sin(x + Time.time);
+            m_allPoints[i].transform.localPosition = new Vector3(x, y, 0);
+        }
+    }
+}
---
+++ Assets/6. Oscillations/Scripts/Oscillations.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Oscillations : MonoBehaviour
+{
+    [SerializeField] private float amplitud=1;
+    [SerializeField] private float period = 1;
+    Vector3 initialPosition;
+
+    private void Start()
+    {
+        initialPosition = new Vector3(transform.position.x, transform.position.y);
+    }
+    private void Update()
+    {
+        //PRIMER EJERCICIO
+        //float x = amplitud * Mathf.Sin(2f * Mathf.PI*(Time.time/period));
+        //transform.position = initialPosition + new Vector3(x, x, 0);
+
+        //SEGUNDO EJERCICIO
+        float x = Mathf.Sin(5f * Time.time) + Mathf.Cos(Time.time / 3f) + Mathf.Sin(Time.time / 13f);
+        transform.position = initialPosition + new Vector3(x, 0, 0);
+    }
+}
